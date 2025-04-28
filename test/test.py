e522541dc01892123cbc8152f48a48347c792b71
Ares-Infenus <imperiodelplastico01@gmail.com>
2025-04-28 02:45:31 -0500

    Arreglando incongruencias.

--- test/test.py
+++
@@ -1,6 +0,0 @@
-from ..src.config import config, ConfigError
-
-# Ejemplo de uso:
-db_host = config["database"]["host"]
-log_dir = config["paths"]["log_dir"]
-log_level = config["other"].get("log_level", "WARNING")
