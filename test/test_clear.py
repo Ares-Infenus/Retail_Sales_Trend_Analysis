f15e073f0276bca232af683784e558de55b3bcd4
Ares-Infenus <imperiodelplastico01@gmail.com>
2025-05-20 13:42:38 -0500

    He creado dos test uno que probara que la limpieza funciones correctamente y el test_db comprobara que funcione correctamente el despliegue y arquitectura del proyecto

---
+++ test/test_clear.py
@@ -0,0 +1,5 @@
+# Comprobaremos que se construya correctamente el docker
+# Comprobaremos que se inicialize correctamente el docker
+# comprobaremos la arquitectura y comsocion de las tagblas de nuestra base de datos
+# Comprobaremos que los scripts de limpieza se inicien de manera adecuada
+# Comprobaremos que los scripts de limpieza se inicien de manera adecuada
